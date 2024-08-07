6590d6b2d828c2d73b51130c5fd10cb77f625423
Salar-ghi <salar.ghi1993@gmail.com>
2024-08-07 16:28:13 +0330

    init

---
+++ src/ApiGatewat/Configuration/CustomAuthorizeFilter.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Filters;
+
+namespace ApiGatewat.Configuration;
+
+public class CustomAuthorizeFilter : IAsyncAuthorizationFilter
+{
+    private readonly IAuthorizationService _authorizationService;
+    public CustomAuthorizeFilter(IAuthorizationService authorizationService)
+    {
+        _authorizationService = authorizationService;
+    }
+
+
+    public async Task OnAuthorizationAsync(AuthorizationFilterContext context)
+    {
+        var result = await _authorizationService.AuthorizeAsync(context.HttpContext.User, null, "MultiAuthPolicy");
+        if (!result.Succeeded)
+        {
+            context.Result = new ForbidResult();
+        }
+        throw new NotImplementedException();
+    }
+}
---
+++ src/ApiGatewat/Program.cs
@@ -0,0 +1,44 @@
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Values;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddOcelot(builder.Configuration);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("MultiAuthPolicy", policy =>
+    {
+        policy.AddAuthenticationSchemes("Scheme1", "Scheme2");
+        policy.RequireAuthenticatedUser();
+    });
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+await app.UseOcelot();
+//app.UseOcelot().Wait();
+
+app.MapControllers();
+
+app.Run();
