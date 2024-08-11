6117d6d2987e655c8f9f5ec732a2c344a4dd31f4
Salar-ghi <salar.ghi1993@gmail.com>
2024-08-11 08:47:35 +0330

    api gateway work correctly

--- src/ApiGatewat/Program.cs
+++ src/ApiGatewat/Program.cs
@@ -4,24 +4,31 @@ using Ocelot.Values;
 
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddOcelot(builder.Configuration);
-
+builder.Logging.AddConsole();
+builder.Logging.SetMinimumLevel(LogLevel.Debug);
 // Add services to the container.
+
+builder.Services.AddCors();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Configuration.AddJsonFile("ocelot.json", optional: false, reloadOnChange: true);
+builder.Services.AddOcelot(builder.Configuration);
 
-builder.Services.AddAuthorization(options =>
-{
-    options.AddPolicy("MultiAuthPolicy", policy =>
-    {
-        policy.AddAuthenticationSchemes("Scheme1", "Scheme2");
-        policy.RequireAuthenticatedUser();
-    });
-});
+
+//builder.Services.AddAuthorization(options =>
+//{
+//    options.AddPolicy("MultiAuthPolicy", policy =>
+//    {
+//        policy.AddAuthenticationSchemes("Scheme1", "Scheme2");
+//        policy.RequireAuthenticatedUser();
+//    });
+//});
+
+
 
 var app = builder.Build();
 
@@ -32,13 +39,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+//app.UseOcelot();
+
+app.UseRouting();
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+//app.UseAuthorization();
+
+app.UseEndpoints(endpoints =>
+{
+    endpoints.MapControllers();
+});
 
 await app.UseOcelot();
-//app.UseOcelot().Wait();
-
-app.MapControllers();
-
 app.Run();
